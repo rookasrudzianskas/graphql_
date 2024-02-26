faf2d7aefff7c686fbe1b661aad04d80f7549009
rokas <rokas@rokas.com>
2024-02-26 11:53:14 +0200

    Add delete and update functionality to user model

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -4,16 +4,22 @@ import {ObjectId} from "mongodb";
 export const typeDef = /* GraphQL */ `
     type Query {
         users: [User!]!
-        user: User
+        user(id: ID!): User
     }
 
     type Mutation {
         createUser(user: NewUserInput!): User
+        deleteUser(id: ID!): Boolean
+        updateUser(id: ID!, update: UpdateUserInput): User
     }
 
     input NewUserInput {
         name: String!
         email: String!
+    }
+
+    input UpdateUserInput {
+        name: String!
     }
 
     type User {
@@ -43,6 +49,18 @@ export const resolvers = {
         ...user,
       };
     },
+    deleteUser: async (_, { id }, { mongo }) => {
+      await mongo.users.deleteOne({ _id: new ObjectId(id) });
+      return true;
+    },
+    updateUser: async (_, { id, update }, { mongo }) => {
+      console.log('update', update);
+      const response = await mongo.users.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: update.name },
+      );
+      return mongo.users.findOne({ _id: new ObjectId(id) });
+    }
   },
 
   User: {

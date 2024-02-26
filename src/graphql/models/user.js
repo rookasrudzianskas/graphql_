71e89afa9851ade9cade33afa0dc1f909449a699
rokas <rokas@rokas.com>
2024-02-26 11:43:35 +0200

    Add email field to NewUserInput in createUser mutation

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -13,7 +13,7 @@ export const typeDef = /* GraphQL */ `
 
     input NewUserInput {
         name: String!
-        age: Int!
+        email: String!
     }
 
     type User {
@@ -37,11 +37,9 @@ export const resolvers = {
 
   Mutation: {
     createUser: async (_, { user }, { mongo }) => {
-      const movies = await mongo.movies.find().toArray();
-      console.log(movies);
-      // insert into DB
+      const response = await mongo.users.insertOne(user);
       return {
-        id: 1,
+        id: response.insertedId,
         ...user,
       };
     },

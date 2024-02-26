3773c0f5d6fd8ce82429f6ca9887703af21e5c2c
rokas <rokas@rokas.com>
2024-02-26 11:02:52 +0200

    Refactor setupDatabase function in mongo index file

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -11,16 +11,15 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
+async function setupDatabase() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    return {
+      client,
+    }
+  } catch (e) {
+    console.error(e);
   }
 }
-run().catch(console.dir);
+setupDatabase().catch(console.dir);
